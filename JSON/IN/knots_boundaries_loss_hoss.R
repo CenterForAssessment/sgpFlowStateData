221c4a4efda11a6ad8e98a4f9c5cc14cdbcc057d
Damian Betebenner <dbetebenner@nciea.org>
2025-05-04 14:35:56 -0400

    Updating method by which achievement data is embedded into sgpFlowStateData

--- JSON/IN/knots_boundaries_loss_hoss.R
+++
@@ -1,5 +0,0 @@
-############################################################
-### Script to export knots, boundaries, loss/hoss to JSON
-############################################################
-
-cat(jsonlite::toJSON(SGP::SGPstateData$IN$Achievement$Knots_Boundaries, pretty=TRUE), file="knots_boundaries_loss_hoss.json")
